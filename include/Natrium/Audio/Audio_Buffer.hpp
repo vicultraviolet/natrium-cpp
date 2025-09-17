248f5f7542ec63e573d1ddcd69c39cfb7318e580
goofyahhvic <95176727+goofyahhvic@users.noreply.github.com>
2025-09-17 16:07:07 -0300

    Created `Audio::Buffer` class

---
+++ include/Natrium/Audio/Audio_Buffer.hpp
@@ -0,0 +1,32 @@
+#if !defined(NA_AUDIO_BUFFER_HPP)
+#define NA_AUDIO_BUFFER_HPP
+
+#include "Natrium/Audio/Wav.hpp"
+
+namespace Na::Audio {
+	class Buffer {
+	public:
+		Buffer(void) = default;
+
+		void destroy(void);
+		~Buffer(void) { this->destroy(); }
+
+		explicit Buffer(WeakRef<Wav> wav);
+
+		Buffer(Buffer&& other) noexcept;
+		Buffer& operator=(Buffer&& other) noexcept;
+
+		[[nodiscard]] u32 frequency(void) const;
+		[[nodiscard]] u32 bit_depth(void) const;
+		[[nodiscard]] u32 channel_count(void) const;
+		[[nodiscard]] u32 size(void) const;
+
+		[[nodiscard]] inline ALuint native(void) const { return m_Buffer; }
+
+		[[nodiscard]] inline operator bool(void) const { return m_Buffer != u32max; }
+	private:
+		ALuint m_Buffer = u32max;
+	};
+} // namespace Na::Audio
+
+#endif // NA_AUDIO_BUFFER_HPP
