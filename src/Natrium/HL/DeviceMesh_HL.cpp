e67b54ab05d4adfe98debb7bdac04d59ce88cc47
goofyahhvic <95176727+goofyahhvic@users.noreply.github.com>
2025-09-23 10:13:53 -0300

    Made `HL::DeviceMesh` an asset

--- src/Natrium/HL/DeviceMesh_HL.cpp
+++ src/Natrium/HL/DeviceMesh_HL.cpp
@@ -2,7 +2,11 @@
 #include "Natrium/HL/DeviceMesh_HL.hpp"
 
 namespace Na::HL {
-	DeviceMesh::DeviceMesh(WeakRef<HostMesh> _host_mesh)
+	DeviceMesh::DeviceMesh(
+		const UUID_t& uuid,
+		WeakRef<const HostMesh> _host_mesh
+	)
+	: Asset(uuid)
 	{
 		auto host_mesh = _host_mesh.lock();
 		NA_ASSERT(host_mesh, "Failed to create DeviceMesh: Invalid host mesh reference!");
@@ -18,10 +22,13 @@ namespace Na::HL {
 	}
 
 	DeviceMesh::DeviceMesh(
+		const UUID_t& uuid,
 		const Vertex* vertices, u32 vertex_count,
 		const u32* indices, u32 index_count
 	)
-	: m_VertexCount(vertex_count), m_IndexCount(index_count)
+	: Asset(uuid),
+	  m_VertexCount(vertex_count),
+	  m_IndexCount(index_count)
 	{
 		m_VertexBuffer = Graphics::MakeVertexBuffer((u64)vertex_count * sizeof(Vertex));
 		m_VertexBuffer->set_data(vertices);
